a687bfa7f8a6dc2a1a34fa41aa429b2250ca85b7
Juan Pablo Garcia <juampigarcia2014@gmail.com>
2019-11-04 17:41:52 -0300

    Update

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 from lexico import *
 from sintactico import *
+import tkinter as tk
 
-user = input('Ingrese usuario: ')
-passw = input('Ingrese contraseña: ')
+def validar():
+    info = tk.Tk()
+    info.title('Info')
+    info.geometry('380x200')
 
-print('\n*** Username')
-if lex_user(user) == True:
-    print('Lexico: Correcto')
-    if sintax_user(user) == True:
-        print('Sintax: Correcto')
+    info1 = tk.Label(info,text='*** Username ***')
+    info2 = tk.Label(info,text='')
+    info3 = tk.Label(info,text='')
+    info1.pack(padx=5,pady=5,ipadx=5,ipady=5)
+    info2.pack()
+    info3.pack()
+
+    if lex_user(entry_user.get()) == True:
+        info2.configure(text='Lexico: Correcto')
+        if sintax_user(entry_user.get()) == True:
+            info3.configure(text='Sintactico: Correcto')
+        else:
+            info3.configure(text='Sintactico: Error')
     else:
-        print('Sintax: Error')
-else:
-    print('Lexico: Error')
+        info2.configure(text='Lexico: Error')
 
-print('\n*** Password')
-if lex_passw(passw) == True :
-    print('Lexico: Correcto')
-    if sintax_passw(passw) == True:
-        print('Sintax: Correcto')
+    info4 = tk.Label(info,text='*** Password ***')
+    info5 = tk.Label(info,text='')
+    info6 = tk.Label(info,text='')
+    info4.pack(padx=5,pady=5,ipadx=5,ipady=5)
+    info5.pack()
+    info6.pack()
+
+    if lex_passw(entry_passw.get()) == True :
+        info5.configure(text='Lexico: Correcto')
+        if sintax_passw(entry_passw.get()) == True:
+            info6.configure(text='Sintactico: Correcto')
+        else:
+            info6.configure(text='Sintactico: Error')
     else:
-        print('Sintax: Error')
-else:
-    print('Lexico: Error')
+        info5.configure(text='Lexico: Error')
+
+    info.mainloop()
+
+
+ventana = tk.Tk()
+ventana.title('Inicio de Sesion')   #Titulo de la ventana
+ventana.geometry('380x200') #Dimension de la ventana (Ancho x Alto)
+e1=tk.Label(ventana,text='Nombre de Usuario',fg='black')
+e1.pack(padx=5,pady=5,ipadx=5,ipady=5)
+entry_user=tk.Entry(ventana)
+entry_user.pack(padx=5,pady=5,ipadx=5,ipady=5)
+e2=tk.Label(ventana,text='Contraseña',fg='black')
+e2.pack(padx=5,pady=5,ipadx=5,ipady=5)
+entry_passw=tk.Entry(ventana)
+entry_passw.pack(padx=5,pady=5,ipadx=5,ipady=5)
+bttn=tk.Button(ventana,text='Enviar',fg='black',command=validar)
+bttn.pack()
+
+ventana.mainloop()  #Linea final de la ventana
